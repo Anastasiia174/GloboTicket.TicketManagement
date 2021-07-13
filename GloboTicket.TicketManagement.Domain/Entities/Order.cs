393837fd6ee7cdb6d633478a9cf63a04e7aec7ed
Anastasiia174 <potapova174@gmail.com>
2021-07-13 15:37:18 +0300

    Initial commit

---
+++ GloboTicket.TicketManagement.Domain/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GloboTicket.TicketManagement.Domain.Common;
+
+namespace GloboTicket.TicketManagement.Domain.Entities
+{
+    public class Order : AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public bool OrderPaid { get; set; }
+    }
+}
