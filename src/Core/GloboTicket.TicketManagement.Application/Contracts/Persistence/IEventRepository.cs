4b4ee436a893670e2fcc011ff7c0076493ff7513
Anastasiia174 <potapova174@gmail.com>
2021-07-13 15:51:37 +0300

    Add core contracts, move to separate folders

---
+++ src/Core/GloboTicket.TicketManagement.Application/Contracts/Persistence/IEventRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GloboTicket.TicketManagement.Domain.Entities;
+
+namespace GloboTicket.TicketManagement.Application.Contracts.Persistence
+{
+    public interface IEventRepository : IAsyncRepository<Event>
+    {
+    }
+}
