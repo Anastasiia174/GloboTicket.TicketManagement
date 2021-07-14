b6d948ffe73965aadab0e25ffcf23082a4014153
Anastasiia174 <potapova174@gmail.com>
2021-07-14 15:49:53 +0300

    Add repositories implementations

---
+++ src/Infrastructure/GloboTicket.TicketManagement.Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GloboTicket.TicketManagement.Persistence.Repositories
+{
+    public class CategoryRepository : BaseRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(GloboTicketDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<List<Category>> GetCategoriesWithEvents(bool includePassedEvents)
+        {
+            var allCategories = await _dbContext.Categories.Include(x => x.Events).ToListAsync();
+
+            if (!includePassedEvents)
+            {
+                allCategories.ForEach(c => c.Events.ToList().RemoveAll(c => c.Date < DateTime.Now));
+            }
+
+            return allCategories;
+        }
+
+        public Task<bool> IsCategoryNameUnique(string name)
+        {
+            var matches = _dbContext.Categories.Any(c => c.Name.Equals(name));
+            return Task.FromResult(matches);
+        }
+    }
+}
