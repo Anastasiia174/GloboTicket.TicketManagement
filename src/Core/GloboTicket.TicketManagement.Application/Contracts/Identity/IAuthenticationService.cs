226dcd93d7e4cdfc1c7cf9ad93e4ada1dab3133f
Anastasiia174 <potapova174@gmail.com>
2021-07-20 09:38:03 +0300

    Add authentication services

---
+++ src/Core/GloboTicket.TicketManagement.Application/Contracts/Identity/IAuthenticationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using GloboTicket.TicketManagement.Application.Models.Authentication;
+
+namespace GloboTicket.TicketManagement.Application.Contracts.Identity
+{
+    public interface IAuthenticationService
+    {
+        Task<AuthenticationResponse> AuthenticateAsync(AuthenticationRequest request);
+
+        Task<RegistrationResponse> RegisterAsync(RegistrationRequest request);
+    }
+}
---
+++ src/Infrastructure/GloboTicket.TicketManagement.Identity/GloboTicketIdentityDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GloboTicket.TicketManagement.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace GloboTicket.TicketManagement.Identity
+{
+    public class GloboTicketIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+    }
+}
